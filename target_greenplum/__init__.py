d4c7e61f34b83e982e81fdfbb361a51803ce5846
lialzm <6356434+lialzm@users.noreply.github.com>
2022-11-02 13:34:49 +0800

    添加调试日志

--- target_greenplum/__init__.py
+++ target_greenplum/__init__.py
@@ -104,6 +104,9 @@ def persist_lines(config, lines) -> None:
             raise
         if 'type' not in o:
             raise Exception("Line is missing required key 'type': {}".format(line))
+        LOGGER.info("line====")
+        LOGGER.info(o)
+        LOGGER.info("line====")
         t = o['type']
 
         if t == 'RECORD':
